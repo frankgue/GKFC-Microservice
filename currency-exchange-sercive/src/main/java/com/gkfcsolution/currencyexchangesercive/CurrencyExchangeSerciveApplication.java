b6f1f38884299a96615c95df5ce7218220dac9db
git <frankguekeng11@gmail.com>
2020-11-21 16:51:07 +0100

    first commit

---
+++ currency-exchange-sercive/src/main/java/com/gkfcsolution/currencyexchangesercive/CurrencyExchangeSerciveApplication.java
@@ -0,0 +1,13 @@
+package com.gkfcsolution.currencyexchangesercive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyExchangeSerciveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CurrencyExchangeSerciveApplication.class, args);
+    }
+
+}
---
+++ currency-exchange-sercive/src/test/java/com/gkfcsolution/currencyexchangesercive/CurrencyExchangeSerciveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gkfcsolution.currencyexchangesercive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyExchangeSerciveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
